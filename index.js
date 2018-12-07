936870d3f4deef0b8811ce388aba06114be6bed3
John S <diaz.john312@gmail.com>
2018-12-07 17:57:59 -0500

    added cors for localhost development

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -8,6 +9,14 @@ const helpers = require('./lib/helpers');
 helpers.cache.flush();
 
 const app = express();
+
+if (process.env.NODE_ENV == 'development') {
+  app.use(cors({
+    origin: 'http://localhost:8080',
+  }));
+}
+
+
 
 app.use(express.static('./client'));
 
@@ -35,7 +44,7 @@ app.get('*', (req, res) => {
   res.status(404).send('404!');
 });
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log('$ SERVER: listening at port ', PORT)
