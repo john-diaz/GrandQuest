68cffd5d92e219268a9bac425498b95c731c3cbd
John S <diaz.john312@gmail.com>
2018-12-09 09:20:09 -0500

    test devlog

--- test/devlog.js
+++ test/devlog.js
@@ -18,84 +18,75 @@ describe('DevLog', () => {
   });
 
   describe('GET /devlog', () => {
+    it('should not be cached', (done) => {
+      redisClient.get(`DB:DEV_LOG`, (err, val) => {
+        expect(val).to.be.null
+        done()
+      });
+    });
     it('should receive an HTML file', (done) => {
       chai.request(server)
         .get('/devlog')
         .end((err, res) => {
           res.should.have.status(200);
-          res.type.should.be.eql('text/html');
+          res.type.should.be.eql('application/json');
 
+          let { data } = res.body
+          data.should.be.an('array');
           done();
-        });
-    });
-  });
-
-  describe('GET /devlog', () => {
-    it('should not be cached', (done) => {
-      redisClient.get(`DB:DEV_LOG`, (err, val) => {
-        expect(val).to.be.null
-        done()
-      });
-    });
-    it('should receive the chosen devlog HTML', (done) => {
-      chai.request(server)
-        .get(`/devlog`)
-        .end((err, res) => {
-          let body = res.body
-
-          expect(body.data).to.be.an('Array');
-          done()
         });
     });
     it('should cache the devlogs', (done) => {
       redisClient.get(`DB:DEV_LOG`, (err, val) => {
-        expect(val).to.be.string
+        expect(val).not.to.be.null
         done()
       });
     });
   });
-  describe('GET /devlog/:id', () => {
-    var devLog;
-    var expectedHTML;
 
-    before((done) => {
-      const query = 'INSERT INTO dev_log (title, log_url) VALUES($1, $2) RETURNING *'
-      const values = ["example log", "week1.html"]
 
-      pool.query(query, values, async (err, results) => {
-        if (err) throw err
-        devLog = results.rows[0];
+  // describe('GET /devlog/:id', () => {
+  //   var devLog;
+  //   var expectedHTML;
+
+  //   before((done) => {
+  //     const query = 'INSERT INTO dev_log (title, log_url) VALUES($1, $2) RETURNING *'
+  //     const values = ["example log", "week1.html"]
+
+  //     pool.query(query, values, async (err, results) => {
+  //       if (err) throw err
+  //       devLog = results.rows[0];
   
-        const { Body } = await s3.getObject({
-          Bucket: 'grandquest-devlog',
-          Key: devLog.log_url,
-        }).promise();
+  //       const { Body } = await s3.getObject({
+  //         Bucket: 'grandquest-devlog',
+  //         Key: devLog.log_url,
+  //       }).promise();
   
-        expectedHTML = Body.toString();
-        done();
-      });
-    });
+  //       expectedHTML = Body.toString();
+  //       done();
+  //     });
+  //   });
 
-    it('should not be cached', (done) => {
-      redisClient.get(`DB:DEV_LOG:${devLog.id}`, (err, val) => {
-        expect(val).to.be.null
-        done()
-      });
-    });
-    it('should receive the chosen devlog HTML', (done) => {
-      chai.request(server)
-        .get(`/devlog/${devLog.id}`)
-        .end((err, res) => {
-          res.body.data.html.should.eq(expectedHTML);
-          done()
-        });
-    });
-    it('should cache the devlog', (done) => {
-      redisClient.get(`DB:DEV_LOG:${devLog.id}`, (err, val) => {
-        expect(val).to.be.string
-        done()
-      });
-    });
-  });
+  //   it('should not be cached', (done) => {
+  //     redisClient.get(`DB:DEV_LOG:${devLog.id}`, (err, val) => {
+  //       expect(val).to.be.null
+  //       done()
+  //     });
+  //   });
+  //   it('should receive the chosen devlog HTML', (done) => {
+  //     chai.request(server)
+  //       .get(`/devlog/${devLog.id}`)
+  //       .end((err, res) => {
+  //         res.body.data.html.should.eq(expectedHTML);
+  //         done()
+  //       });
+  //   });
+  //   it('should cache the devlog', (done) => {
+  //     redisClient.get(`DB:DEV_LOG:${devLog.id}`, (err, val) => {
+  //       expect(val).to.be.string
+  //       done()
+  //     });
+  //   });
+  // });
 
 });
