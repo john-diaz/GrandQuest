6950bc6acfcca4ed242a305f0a639152723ae369
John S <diaz.john312@gmail.com>
2018-12-12 20:05:41 -0500

    add routes, cors config, aws config and redis client to app

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require('express');
-const cors = require('cors');
+const app = express();
 
 require('dotenv').config();
 
 const helpers = require('./lib/helpers');
 helpers.cache.flush();
 
-const app = express();
-
+const cors = require('cors');
+// enable CORS
 if (process.env.NODE_ENV == 'development') {
   app.use(cors({
     origin: 'http://localhost:8080',
   }));
 }
 
+// parse json body
 app.use(express.static('./client'));
 
-require('./lib/routes/devlog')(app);
-
-// VIEWS
+// config routes
+const devLogRoutes = require('./lib/routes/devlog');
+const forumRoutes = require('./lib/routes/forum');
+app.use(devLogRoutes);
+app.use(forumRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello, world!' });
@@ -28,7 +31,7 @@ app.get('*', (req, res) => {
   res.status(404).send({ message: 'This route does not exist' });
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log('$ SERVER: listening at port ', PORT)
