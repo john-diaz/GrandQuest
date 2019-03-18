ce23048959d282952d00caea2227f494d3c5f0da
John S <diaz.john312@gmail.com>
2019-03-18 16:56:46 -0400

    add defense / power increase transactions

---
+++ packages/data/costs.js
@@ -0,0 +1,15 @@
+/* ./packages/data/costs.js */
+
+const powerDefenseCosts = {
+	1: 0,
+	2: 80,
+	3: 140,
+	4: 200,
+	5: 280,
+	6: 330,
+	7: 380,
+};
+
+module.exports = {
+	powerDefenseCosts,
+}
--- packages/socketServer/events/shops.js
+++ packages/socketServer/events/shops.js
@@ -3,6 +3,8 @@
 const store = require('../../engine/store');
 const pool = require('../../services/psql/pool');
 
+const { powerDefenseCosts } = require('../../data/costs');
+
 const availableShops = {
 	'potions-shop': {
 		location: 'monokai-village',
@@ -62,6 +64,113 @@ const availableShops = {
 								before: health,
 								after: health + added,
 								max: maxHealth,
+								cost: cost,
+							});
+						});
+					}
+				});
+			}),
+		}
+	},
+	'combat-shop': {
+		location: 'monokai-village',
+		items: {},
+		actions: {
+			'power-up': (user) => new Promise((resolve, reject) => {
+				pool.query(`SELECT * FROM combatants WHERE id = ${user.id}`, (err, results) => {
+					if (err) {
+						reject('Something went wrong while working on your upgrade. Please try again later');
+					} else {
+						const combatant = results.rows[0];
+						const currentPower = combatant.power;
+						const nextPower = currentPower + 1;
+
+						if (!powerDefenseCosts.hasOwnProperty(nextPower)) {
+							return reject('Theres not much I can do for your power right now. Come back later.');
+						}
+						const cost = powerDefenseCosts[nextPower];
+
+						if (user.gold < cost) {
+							return reject('You dont have enough gold to afford this upgrade. Come back later.');
+						}
+
+						const newGold = Math.round(user.gold - cost);
+
+						pool.query(`
+							WITH u AS (
+			          UPDATE users
+			          SET gold = ${newGold}
+			          WHERE id = ${user.id}
+			          RETURNING id
+			        )
+			        UPDATE combatants
+			        SET power = power + 1
+			        WHERE id = ${user.id}
+						`, (err, results) => {
+							if (err) {
+								return reject('Something went wrong while upgrading defense. Please try again later');
+							}
+							store.update('users', (users) => ({
+								...users,
+								[user.id]: {
+									...user,
+									gold: newGold,
+								}
+							}));
+							resolve({
+								before: currentPower,
+								after: nextPower,
+								cost: cost,
+							});
+						});
+					}
+				});
+			}),
+			'defense-up': (user) => new Promise((resolve, reject) => {
+				pool.query(`SELECT * FROM combatants WHERE id = ${user.id}`, (err, results) => {
+					if (err) {
+						reject('Something went wrong while working on your upgrade. Please try again later');
+					} else {
+						const combatant = results.rows[0];
+						const currentDefense = combatant.defense;
+						const nextDefense = currentDefense + 1;
+
+						if (!powerDefenseCosts.hasOwnProperty(nextDefense)) {
+							return reject('Theres not much I can do for your defense right now. Come back later.');
+						}
+						const cost = powerDefenseCosts[nextDefense];
+
+						if (user.gold < cost) {
+							return reject('You dont have enough gold to afford this upgrade. Come back later.');
+						}
+
+						const newGold = Math.round(user.gold - cost);
+
+						pool.query(`
+							WITH u AS (
+			          UPDATE users
+			          SET gold = ${newGold}
+			          WHERE id = ${user.id}
+			          RETURNING id
+			        )
+			        UPDATE combatants
+			        SET defense = defense + 1
+			        WHERE id = ${user.id}
+						`, (err, results) => {
+							if (err) {
+								return reject('Something went wrong while upgrading defense. Please try again later');
+							}
+
+							store.update('users', (users) => ({
+								...users,
+								[user.id]: {
+									...user,
+									gold: newGold,
+								}
+							}));
+							resolve({
+								before: currentDefense,
+								after: nextDefense,
 								cost: cost,
 							});
 						});
